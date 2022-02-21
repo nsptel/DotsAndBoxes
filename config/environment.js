c8ac9a07ddb001513f271dfcb7ae989d6cbd6b1f
Nilkumar Patel <nilkumarpatel48@gmail.com>
2022-02-21 13:31:42 -0500

    Adds modulePrefix in config

--- config/environment.js
+++ config/environment.js
@@ -2,7 +2,7 @@
 
 module.exports = function (environment) {
   let ENV = {
-    modulePrefix: 'dots-and-boxes',
+    modulePrefix: 'DotsAndBoxes',
     environment,
     rootURL: '/',
     locationType: 'auto',
