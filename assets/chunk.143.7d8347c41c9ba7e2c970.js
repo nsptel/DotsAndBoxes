b240078a505d8a32b79dea286031b1b6f45e0273
Nilkumar Patel <nilkumarpatel48@gmail.com>
2022-02-21 16:58:12 -0500

    Initial gh-pages release

---
+++ assets/chunk.143.7d8347c41c9ba7e2c970.js
@@ -0,0 +1,8 @@
+var __ember_auto_import__;(()=>{var o={856:(o,e,r)=>{var n,l
+o.exports=(n=_eai_d,l=_eai_r,window.emberAutoImportDynamic=function(o){return 1===arguments.length?l("_eai_dyn_"+o):l("_eai_dynt_"+o)(Array.prototype.slice.call(arguments,1))},window.emberAutoImportSync=function(o){return l("_eai_sync_"+o)(Array.prototype.slice.call(arguments,1))},void n("ember",[],(function(){return r(600)})))},852:function(o,e){window._eai_r=require,window._eai_d=define},600:()=>{console.log("===================================================================="),console.log(""),console.log("  The `ember` node module is a placeholder, you may be looking for:"),console.log(""),console.log("  * `ember-cli` (the command line tool) "),console.log("  * `ember-source` (the framework code) "),console.log(""),console.log("  Visit http://emberjs.com/ for more details"),console.log(""),console.log("====================================================================")}},e={}
+function r(n){var l=e[n]
+if(void 0!==l)return l.exports
+var t=e[n]={exports:{}}
+return o[n].call(t.exports,t,t.exports,r),t.exports}r(852)
+var n=r(856)
+__ember_auto_import__=n})()
